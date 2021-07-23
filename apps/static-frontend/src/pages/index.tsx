aa5d734411bdca98e89c01b966c6434e6ea45811
Sergey Ivanov <sergeydev90@gmail.com>
2021-07-23 03:12:04 -0400

    initial setup

---
+++ apps/static-frontend/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { ReactComponent as Logo } from './logo.svg';
+import star from './star.svg';
+
+export function Index() {
+  return <h1>Welcome to static-frontend!</h1>;
+}
+
+export default Index;
--- libs/api-interfaces/README.md
+++ libs/api-interfaces/README.md
@@ -1,3 +1,3 @@
 # api-interfaces
 
-This library was generated with [Nx](https://nx.dev).
+Interfaces that describe API requests and responses.
