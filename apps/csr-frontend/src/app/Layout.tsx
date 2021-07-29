f49d98f989f2be784f35dad1efedf68683c1e809
Sergey Ivanov <sergeydev90@gmail.com>
2021-07-29 10:56:49 -0400

    fixed csr frontend tailwind config

---
+++ apps/csr-frontend/src/app/Layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../features/footer/Footer';
+import NavBar from '../features/navbar/Navbar'
+
+
+const Layout: React.FC = ({ children }) => {
+    return (
+        <>
+            <main className="w-screen">{children}</main>
+            <Footer />
+        </>
+    )
+}
+
+export default Layout;
