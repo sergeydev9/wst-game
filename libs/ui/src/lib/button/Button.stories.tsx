964fbb3fd6441a274591df3b6dde33188c4c86a8
Sergey Ivanov <sergeydev90@gmail.com>
2021-08-01 15:09:32 -0400

    added fontSize and border thickness to button. Upgraded docs

--- libs/ui/src/lib/button/Button.stories.tsx
+++ libs/ui/src/lib/button/Button.stories.tsx
@@ -7,76 +7,129 @@ export default {
     title: "Buttons",
     argTypes: {
         color: {
-            type: 'select',
+            name: 'color',
+            default: "primary",
+            description: "Applies a theme color to the element.",
+            control: {
+                type: 'select',
+            },
+            table: {
+                type: { required: false, summary: "Theme Color", detail: `"${THEME_COLORS.join('" \n|"')}" \n| undefined` },
+                defaultValue: { summary: "primary" },
+            },
             options: THEME_COLORS, // imports from util lib
-            default: 'primary'
+        },
+        fontSize: {
+            name: "fontSize",
+            default: "headline",
+            description: "Selects a theme preset font-size for the button label",
+            table: {
+                type: { summary: "Font Size", detail: '"headline" | "label-big" | "label-small"' },
+                defaultValue: { summary: "headline" }
+            },
+            control: {
+                type: 'select',
+            },
+            options: ["headline", "label-big", "label-small"]
+        },
+        border: {
+            name: "border",
+            default: undefined,
+            description: "If specified, gives the button a white background, changes the text color, and sets the border to the desired thickness",
+            table: {
+                type: { summary: "thickness", detail: '"thin" | "medium" | "thick" | undefined' },
+                defaultValue: { summary: 'undefined' }
+            },
+            control: {
+                type: 'select'
+            },
+            options: ['thin', 'medium', 'thick', undefined],
+
         },
         $small: {
+            name: '$small',
+            description: "Sets the size of the button",
             type: 'boolean',
-            default: false
-        },
-        $border: {
-            type: 'boolean',
-            default: false
+            default: false,
+            table: {
+                type: { summary: 'boolean' },
+                defaultValue: { summary: 'false' }
+            },
         },
         $pill: {
+            name: '$pill',
+            description: 'Makes the button pill shaped',
             type: 'boolean',
-            default: false
+            default: false,
+            table: {
+                type: { summary: 'boolean' },
+                defaultValue: { summary: false }
+            }
         },
         boxshadow: {
-            type: 'select',
-            options: ['shadow-sm', 'shadow', 'shadow-md', 'shadow-lg', 'shadow-xl', 'shadow-2xl']
-        }
-
-    },
-    parameters: {
-        docs: {
-            source: {
-                type: 'code'
+            name: 'boxshadow',
+            description: 'Applies box-shadow presets to the element',
+            type: 'string',
+            control: {
+                type: 'select',
+            },
+            options: ['shadow-sm', 'shadow', 'shadow-md', 'shadow-lg', 'shadow-xl', 'shadow-2xl', undefined],
+            table: {
+                type: { summary: 'Box-Shadow Preset', detail: '"shadow-sm" | "shadow" | "shadow-md" | "shadow-lg" | "shadow-xl" | "shadow-2xl" | undefined' },
+                defaultValue: { summary: 'undefined' }
             }
         }
-    }
+    },
+
 
 
 } as Meta;
 
-const Template: Story<ButtonProps> = ({ color, $small, $border, $pill, boxshadow }) => (
-    <Button color={color} $small={$small} $pill={$pill} $border={$border} boxshadow={boxshadow}>Button Component</Button>
+const Template: Story<ButtonProps> = ({ color, fontSize, border, boxshadow, $small, $pill }) => (
+    <Button
+        color={color}
+        fontSize={fontSize}
+        border={border}
+        boxshadow={boxshadow}
+        $small={$small}
+        $pill={$pill}>
+        Button Component
+    </Button>
 )
 
 export const ButtonComponent = Template.bind({});
 ButtonComponent.args = {
     color: "primary",
+    fontSize: "headline",
     boxshadow: undefined,
     $small: false,
-    $border: false,
     $pill: false,
-
+    border: undefined,
 }
 
 export const SolidSubtleStroke = () => <Button color="subtle-stroke">Subtle Stroke</Button>
 
-export const BorderPrimary = () => <Button color="primary" $border>Border</Button>
+export const BorderPrimary = () => <Button border="medium">Border</Button>
 
-export const BorderSubtleStroke = () => <Button color="subtle-stroke" $border>Border Subtle Stroke</Button>
+export const BorderSubtleStroke = () => <Button color="subtle-stroke" border="medium">Border Subtle Stroke</Button>
 
-export const SolidSmall = () => <Button color="primary" $small>Solid Small</Button>
+export const SolidSmall = () => <Button $small>Solid Small</Button>
 
-export const BorderSmall = () => <Button color="primary" $border $small>Border Small</Button>
+export const BorderSmall = () => <Button border="medium" $small>Border Small</Button>
 
-export const LargeSolidPill = () => <Button color="primary" $pill>Large Solid Pill</Button>
+export const LargeSolidPill = () => <Button $pill>Large Solid Pill</Button>
 
-export const LargeBorderPill = () => <Button color="primary" $pill $border>Large Border Pill</Button>
+export const LargeBorderPill = () => <Button border="medium" $pill >Large Border Pill</Button>
 
-export const SmallSolidPill = () => <Button color="primary" $pill $small>Small Solid Pill</Button>
+export const SmallSolidPill = () => <Button $pill $small>Small Solid Pill</Button>
 
-export const SmallBorderPill = () => <Button color="primary" $pill $small $border>Small</Button>
+export const SmallBorderPill = () => <Button border="medium" $pill $small >Small</Button>
 
-export const BoxShadowPrimary = () => {
+export const boxshadowPrimary = () => {
     // ignore containing div
     return (
         <div className="bg-blue-200 py-8 flex justify-center flex-row">
-            <Button color="primary" boxshadow="shadow-lg" $pill $border>Large pill with box shadow</Button>
+            <Button color="primary" boxshadow="shadow-lg" border="medium" $pill >Large pill with box shadow</Button>
         </div>
     )
 }
--- libs/ui/src/lib/button/Button.tsx
+++ libs/ui/src/lib/button/Button.tsx
@@ -2,24 +2,54 @@ import tw from "tailwind-styled-components";
 import { ThemeColor } from "@whosaidtrue/app-interfaces";
 import { genTextColor, genBorderColor, genBgColor } from "@whosaidtrue/util"
 
+type BorderThickness = "thin" | "medium" | "thick" | undefined;
+type FontSize = "label-big" | "label-small" | "headline"; // Font sizes corresponding to the names in the design language
+
 export interface ButtonProps {
-    color: ThemeColor;
-    $border?: boolean;
+    color?: ThemeColor;
+    fontSize?: FontSize;
+    border?: BorderThickness;
     $small?: boolean;
     $pill?: boolean;
     boxshadow?: 'shadow-sm' | 'shadow' | 'shadow-md' | 'shadow-lg' | 'shadow-xl' | 'shadow-2xl'
 }
 
-const colorHelper = (color: ThemeColor, border: boolean | undefined): string => {
-    const borderBase = "border-2 bg-white";
-    return border ? `${borderBase} ${genTextColor(color)} ${genBorderColor(color)}` : `${genBgColor(color)} text-white`
+const borderHelper = (border: "thin" | "medium" | "thick"): string => {
+    switch (border) {
+        case "thin":
+            return "border"
+        case "medium":
+            return "border-2"
+        case "thick":
+            return "border-4"
+    }
+}
+
+const fontSizeHelper = (size: FontSize | undefined): string => {
+    switch (size) {
+        case "label-big":
+            return "text-label-big"
+        case "label-small":
+            return "text-label-small"
+        case "headline":
+            return "text-headline"
+        default:
+            return "text-headline"
+    }
+}
+
+const colorHelper = (color: ThemeColor | undefined, border: BorderThickness): string => {
+    return (border ?
+        `bg-white ${borderHelper(border)} ${(color && genTextColor(color)) || "text-primary"} ${(color && genBorderColor(color)) || "border-primary"}` // if border is specified
+        : `${(color && genBgColor(color)) || "bg-primary"} text-white` // if no border specified, i.e. default
+    )
 }
 
 export default tw.button<ButtonProps>`
-    box-content
+    ${(p) => fontSizeHelper(p.fontSize)}
     ${(p) => p.boxshadow ? p.boxshadow : ""}
     ${(p) => p.$small ? "py-1 px-2 text-sm" : "py-4 px-8"}
-    ${(p) => colorHelper(p.color, p.$border)}
+    ${(p) => colorHelper(p.color, p.border)}
     ${(p) => p.$pill ? "rounded-full" : "rounded-lg"}
 
 
