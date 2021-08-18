9671e09a31390bea27a3e9798091675b71f6aa14
Sergey Ivanov <sergeydev90@gmail.com>
2021-08-17 21:45:02 -0400

    player name tests pass

--- libs/data/src/lib/game-players/GamePlayers.spec.ts
+++ libs/data/src/lib/game-players/GamePlayers.spec.ts
@@ -3,17 +3,21 @@ import { TEST_DB_CONNECTION } from '@whosaidtrue/util';
 import { setupGame } from '../util/testDependencySetup';
 import { cleanDb } from '../util/cleanDb';
 import TEST_GAME_PLAYERS from '../test-objects/gamePlayers';
+import TEST_GAMES from '../test-objects/games';
 import GamePlayers from './GamePlayers';
+import Games from '../games/Games.dao';
 
 describe('GamePlayers dao', () => {
     let pool: Pool;
     let players: GamePlayers;
     let game_id: number;
-    let deck_id: number;
+    let deck_id: number
+    let games: Games;
 
     beforeAll(async () => {
         pool = new Pool(TEST_DB_CONNECTION);
         players = new GamePlayers(pool);
+        games = new Games(pool);
     })
 
     beforeEach(async () => {
@@ -44,9 +48,48 @@ describe('GamePlayers dao', () => {
                 expect(e).toEqual(expected)
             }
         })
-        // it('should throw if player name already taken for game', async () => {})
+        it('should throw if player name already taken for game', async () => {
+            const player = TEST_GAME_PLAYERS[0];
+            await players.insertOne({ ...player, game_id })
+
+            try {
+                await players.insertOne({ ...player, game_id })
+            } catch (e) {
+                const expected = new DatabaseError('duplicate key value violates unique constraint "game_players_game_id_player_name_unique_index"', 1, 'error');
+                expect(e).toEqual(expected)
+            }
+        })
+
+        it('should throw if names differ only in case', async () => {
+            const player = TEST_GAME_PLAYERS[0];
+            await players.insertOne({ ...player, game_id })
+
+            try {
+                const { player_name } = player;
+                player_name.toUpperCase();
+                await players.insertOne({ ...player, game_id, player_name })
+            } catch (e) {
+                const expected = new DatabaseError('duplicate key value violates unique constraint "game_players_game_id_player_name_unique_index"', 1, 'error');
+                expect(e).toEqual(expected)
+            }
+        })
+
+        it('should succeed if same name, different game', async () => {
+            // create a second game
+            const access_code = 'gameplayertest'
+            const gameResult = await games.insertOne({ ...TEST_GAMES[0], deck_id, access_code });
+
+
+            // insert first player
+            const player = TEST_GAME_PLAYERS[0];
+            const result1 = await players.insertOne({ ...player, game_id });
+
+            // insert second player into second game, using the same name
+            game_id = gameResult.rows[0].id;
+            const { rows } = await players.insertOne({ ...player, game_id });
+            expect(rows[0].id).not.toEqual(result1.rows[0].id);
+        })
 
     })
 
-    // describe('setHost', () => {})
 })
