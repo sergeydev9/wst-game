c4c85296bc88767cbb4586b7a90a8c4d9de8e176
Sergey Ivanov <sergeydev90@gmail.com>
2021-08-30 22:55:42 -0700

    fixup App.ts to match api/app.ts better

--- apps/socket-server/src/app/App.ts
+++ apps/socket-server/src/app/App.ts
@@ -1,29 +1,17 @@
-import * as express from 'express';
-import * as expressWs from 'express-ws';
-
-import * as helmet from "helmet";
-import * as compression from 'compression';
-import * as cookieParser from 'cookie-parser';
-import * as cors from 'cors';
-import * as hpp from 'hpp';
-import * as morgan from 'morgan';
-
-import {logger, stream} from "@whosaidtrue/logger";
-
+import express from 'express';
+import expressWs from 'express-ws';
+import helmet from "helmet";
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import hpp from 'hpp';
+import { logger } from '@whosaidtrue/logger';
 
 class App {
-    private readonly wss: expressWs.Instance;
-    private readonly app: expressWs.Application;
-
-    private readonly port: string | number;
-    private readonly env: string;
+    private readonly wss = expressWs(express());
+    private readonly app =  this.wss.app;
+    private readonly port = process.env.PORT || 4001;
 
     constructor() {
-        this.wss = expressWs(express());
-        this.app = this.wss.app;
-        this.port = process.env.PORT || 4001;
-        this.env = process.env.NODE_ENV || 'development';
-
         this.initializeMiddlewares();
     }
 
@@ -34,27 +22,19 @@ class App {
     }
 
     private initializeMiddlewares() {
-        if (this.env === 'production') {
-            this.app.use(morgan('combined', {stream}));
-            this.app.use(cors({origin: 'your.domain.com', credentials: true}));
-        } else {
-            this.app.use(morgan('dev', {stream}));
-            this.app.use(cors({origin: true, credentials: true}));
-        }
-
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
+        this.app.use(cookieParser());
+        this.app.use(cors({ origin: process.env.DOMAIN || true, credentials: true }));
         this.app.use(hpp());
         this.app.use(helmet());
-        this.app.use(compression());
-        this.app.use(express.json());
-        this.app.use(express.urlencoded({extended: true}));
-        this.app.use(cookieParser());
     }
 
     public listen() {
         this.app.listen(this.port, () => {
             logger.info(`=================================`);
-            logger.info(`======= ENV: ${this.env} =======`);
-            logger.info(`🚀 Socket Server listening on the port ${this.port}`);
+            logger.info(`======= ENV: ${process.env.NODE_ENV} =======`);
+            logger.info(`🚀 App listening on the port ${this.port}`);
             logger.info(`=================================`);
         });
     }
