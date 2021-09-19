6f850b1d8669d378379af20ca66daeafb7942bfc
Sergey Ivanov <sergeydev90@gmail.com>
2021-09-19 12:16:01 -0400

    tweaked spacing on deck card

--- libs/ui/src/lib/decks/deck-card/DeckCard.tsx
+++ libs/ui/src/lib/decks/deck-card/DeckCard.tsx
@@ -11,7 +11,7 @@ export interface DeckCardProps extends React.HtmlHTMLAttributes<HTMLDivElement> 
 const DeckCard: React.FC<DeckCardProps> = ({ name, movieRating, thumbnailUrl, noPointer, ...rest }) => {
     return (
         <div className=" inline-block  filter drop-shadow-card py-4 sm:px-4 bg-purple-card-bg w-64 h-64 sm:w-72 sm:h-72 rounded-3xl cursor-pointer mx-1 my-4 sm:mx-4 select-none" {...rest}>
-            <img className={`${noPointer ? '' : 'cursor-pointer'} w-max block mb-6 mx-auto`} src={thumbnailUrl} width="240px" height="160px" alt="Deck Thumbnail" />
+            <img className={`${noPointer ? '' : 'cursor-pointer'} w-max block mb-8 mx-auto`} src={thumbnailUrl} width="240px" height="160px" alt="Deck Thumbnail" />
             <div className="w-full flex items-center justify-center">
                 <h2 className="text-basic-black font-bold text-md text-center mr-6 flex-initial">{name}</h2>
                 <RatingLabel className="flex-none" rating={movieRating} />
