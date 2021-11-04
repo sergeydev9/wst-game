e2d11dc22f5c429b5c12a3f7a43623b255105677
Sergey Ivanov <sergeydev90@gmail.com>
2021-11-04 18:14:30 -0400

    added close on redis connection error to api and socket-server

--- apps/api/src/app/redis.ts
+++ apps/api/src/app/redis.ts
@@ -1,4 +1,5 @@
 import Redis from 'ioredis';
+import { logger } from '@whosaidtrue/logger';
 
 // create Redis client
 export const redisClient = new Redis({
@@ -7,3 +8,11 @@ export const redisClient = new Redis({
     family: 4,
     db: 0
 });
+
+redisClient.on("error", err => {
+    logger.fatal({
+        message: 'Error in redis connection. Shutting api down.',
+        err
+    });
+    process.exit(1);
+})
--- apps/socket-server/src/redis.ts
+++ apps/socket-server/src/redis.ts
@@ -1,10 +1,29 @@
 import Redis from 'ioredis';
+import { logger } from '@whosaidtrue/logger';
 
 // create Redis client
 export const pubClient = new Redis({
     host: process.env.REDIS_HOST,
     port: 6379,
     family: 4,
-    db: 0
+    db: 0,
+    keepAlive: 1,
+
 });
+
+pubClient.on("error", err => {
+    logger.fatal({
+        message: 'Error in redis connection. Shutting socket-server down.',
+        err
+    });
+    process.exit(1);
+})
 export const subClient = pubClient.duplicate();
+
+subClient.on("error", err => {
+    logger.fatal({
+        message: 'Error in redis connection. Shutting socket-server down',
+        err
+    });
+    process.exit(1);
+})
