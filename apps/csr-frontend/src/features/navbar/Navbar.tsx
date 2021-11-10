1ebcb3cab8d15be9c132ac9362f40c5f4050db04
Sergey Ivanov <sergeydev90@gmail.com>
2021-11-10 11:13:37 -0600

    updated logo used in Navbar component

--- apps/csr-frontend/src/features/navbar/Navbar.tsx
+++ apps/csr-frontend/src/features/navbar/Navbar.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import classNames from 'classnames';
-import { NavLogo } from '@whosaidtrue/ui';
 import { useAppSelector } from '../../app/hooks';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
 import { selectAccessCode, selectPlayerName } from '../game/gameSlice';
 import InGameNav from './InGameNav';
 import LargeNav from './LargeNav';
@@ -13,20 +13,20 @@ const NavBar: React.FC = () => {
   const location = useLocation();
   const inGame = playerName || gameCode;
 
-  const componentClassName = classNames(
-    'w-full flex flex-row mb-20 items-center rounded-b-3xl overscroll-contain h-20 px-5',
-    location.pathname === '/' && !inGame
-      ? 'bg-transparent justify-end'
-      : 'bg-purple-subtle-fill justify-between'
-  );
-
   return (
-    <nav className={componentClassName}>
+    <nav
+      className={classNames(
+        'w-full flex flex-row mb-20 items-center rounded-b-3xl overscroll-contain h-20 px-5',
+        location.pathname === '/' && !inGame
+          ? 'bg-transparent justify-end'
+          : 'bg-purple-subtle-fill justify-between'
+      )}
+    >
       <Link
         className={location.pathname === '/' && !inGame ? 'sr-only' : ''}
         to="/"
       >
-        <NavLogo />
+        <Logo className="w-14 h-14" />
       </Link>
       {inGame ? <InGameNav /> : <LargeNav />}
     </nav>
