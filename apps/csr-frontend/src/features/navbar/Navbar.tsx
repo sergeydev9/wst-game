6c8826537aae009223b40b5b8acd8898c678feea
Sergey Ivanov <sergeydev90@gmail.com>
2021-11-10 09:12:38 -0600

    brand logo hidden on home page

--- apps/csr-frontend/src/features/navbar/Navbar.tsx
+++ apps/csr-frontend/src/features/navbar/Navbar.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import classNames from 'classnames';
 import { NavLogo } from '@whosaidtrue/ui';
-import LargeNav from './LargeNav';
 import { useAppSelector } from '../../app/hooks';
 import { selectAccessCode, selectPlayerName } from '../game/gameSlice';
 import InGameNav from './InGameNav';
+import LargeNav from './LargeNav';
 
 const NavBar: React.FC = () => {
-    const gameCode = useAppSelector(selectAccessCode);
-    const playerName = useAppSelector(selectPlayerName);
-    const location = useLocation();
-    const inGame = (playerName || gameCode)
+  const gameCode = useAppSelector(selectAccessCode);
+  const playerName = useAppSelector(selectPlayerName);
+  const location = useLocation();
+  const inGame = playerName || gameCode;
 
-    return (
-        <nav className={`w-full flex flex-row justify-between mb-20 items-center ${location.pathname === '/' && !inGame ? 'bg-transparent' : 'bg-purple-subtle-fill'} rounded-b-3xl overscroll-contain h-20 px-5`}>
-            <Link to="/"><NavLogo /></Link>
-            {inGame ? <InGameNav /> : <LargeNav />}
-        </nav>
-    )
-}
+  const componentClassName = classNames(
+    'w-full flex flex-row mb-20 items-center rounded-b-3xl overscroll-contain h-20 px-5',
+    location.pathname === '/' && !inGame
+      ? 'bg-transparent justify-end'
+      : 'bg-purple-subtle-fill justify-between'
+  );
+
+  return (
+    <nav className={componentClassName}>
+      <Link
+        className={location.pathname === '/' && !inGame ? 'sr-only' : ''}
+        to="/"
+      >
+        <NavLogo />
+      </Link>
+      {inGame ? <InGameNav /> : <LargeNav />}
+    </nav>
+  );
+};
 
 export default NavBar;
