564a7ee531342e9f3622eeac46d5225f9482e129
Sergey Ivanov <sergeydev90@gmail.com>
2021-11-29 10:22:51 -0500

    fixed in game name positioning in navbar

--- apps/csr-frontend/src/features/navbar/InGameNav.tsx
+++ apps/csr-frontend/src/features/navbar/InGameNav.tsx
@@ -1,10 +1,7 @@
-import { lazy, Suspense } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import Loading from '../loading/Loading';
 import { selectPlayerName, selectAccessCode } from '../game/gameSlice';
-import { Button, Modal, NoFlexBox, Box } from '@whosaidtrue/ui';
+import { Button } from '@whosaidtrue/ui';
 import { setFullModal } from '../modal/modalSlice';
-import GameOptionsModal from '../modal/full-screen-modals/GameOptionsModal';
 
 
 const InGameNav: React.FC = () => {
@@ -20,7 +17,7 @@ const InGameNav: React.FC = () => {
     // if there is a name, then show it. Otherwise show the game code
     return (
         <>
-            <h2 className="text-basic-black font-extrabold relative mx-auto text-2xl leading-tight">{name ? name : `Game Code: ${accessCode}`}</h2>
+            <h2 className="text-basic-black font-extrabold relative mx-auto text-center text-lg sm:text-2xl leading-tight">{name ? name : `Game Code: ${accessCode}`}</h2>
             <Button type="button" buttonStyle='small' $secondary onClick={openGameOptions}>Game Options</Button>
 
         </>
